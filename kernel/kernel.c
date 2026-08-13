4fee0582a039113af33ddd2911c1c2b9a4522ce1
Rahul Dey <148911682+rahul5162Volt@users.noreply.github.com>
2026-08-13 08:13:20 +0530

    Add debug kernel cursor controls

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,28 +1,48 @@
+#include "vga/vga.h"
+#include "keyboard/keyboard.h"
+
 void kernel_main()
 {
+    vga_clear();
+    vga_print("MyOS\n");
+    vga_print("Keyboard ready.");
 
-    char* vga=(char*)0xB8000;
-
-
-    vga[0]='K';
-    vga[1]=0x07;
-
-
-    vga[2]='E';
-    vga[3]=0x07;
-
-
-    vga[4]='R';
-    vga[5]=0x07;
-
-
-    vga[6]='N';
-    vga[7]=0x07;
-
-
-    while(1)
+    while (1)
     {
+        if (keyboard_has_data())
+        {
+            unsigned char scancode = keyboard_read_scancode();
 
+            keyboard_update_state(scancode);
+
+            if (!keyboard_is_release(scancode))
+            {
+                if (keyboard_is_arrow_left(scancode))
+                    vga_cursor_left();
+                else if (keyboard_is_arrow_right(scancode))
+                    vga_cursor_right();
+                else if (keyboard_is_arrow_up(scancode))
+                    vga_cursor_up();
+                else if (keyboard_is_arrow_down(scancode))
+                    vga_cursor_down();
+                else if (keyboard_is_tab(scancode))
+                {
+                    vga_put_char(' ');
+                    vga_put_char(' ');
+                    vga_put_char(' ');
+                    vga_put_char(' ');
+                }
+                else if (keyboard_is_enter(scancode))
+                    vga_put_char('\n');
+                else if (keyboard_is_backspace(scancode))
+                    vga_backspace();
+                else
+                {
+                    char character = keyboard_scancode_to_ascii(scancode);
+                    if (character != '\0')
+                        vga_put_char(character);
+                }
+            }
+        }
     }
-
 }
