7e899e1769d55f22c4a9da6290822ea91f895be7
Rahul <ironlightrahul@gmail.com>
2026-09-19 11:24:45 +0530

    Remove startup wait

--- os/startup/os_startup.c
+++ os/startup/os_startup.c
@@ -1,14 +1,11 @@
 #include "os_startup.h"
 
 #include "os_startup_screen.h"
-#include "os_startup_wait.h"
 #include "os_main.h"
 
 void os_startup_run(void)
 {
     os_startup_screen_show();
-
-    os_startup_wait();
 
     os_main_run();
 }
--- os/startup/os_startup_wait.h
+++
@@ -1,6 +0,0 @@
-#ifndef OS_STARTUP_WAIT_H
-#define OS_STARTUP_WAIT_H
-
-void os_startup_wait(void);
-
-#endif
